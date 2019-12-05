065c7e4d9413173cf4defae1e688025996d12276
JugurthaT <aimen_r@yahoo.fr>
2019-12-05 16:51:42 +0300

    Update static_code_analysis.groovy

--- sonarqube/static_code_analysis.groovy
+++ sonarqube/static_code_analysis.groovy
@@ -1,5 +1,11 @@
 void call(){
-  println "sonarqube: static_code_analysis()"
-  println "I have applied all the needed tests and we can go ahead"
+  agent { docker 'maven:3-alpine' } 
+    stages {
+        stage('Example Build') {
+            steps {
+                sh 'mvn -B clean verify'
+            }
+        }
+    }
 }
 
