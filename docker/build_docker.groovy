35deedaa0558d0c8cf1961586a9d3caf36b1ba85
JugurthaT <aimen_r@yahoo.fr>
2019-12-05 10:56:05 +0300

    Update build_docker.groovy

--- docker/build_docker.groovy
+++ docker/build_docker.groovy
@@ -7,4 +7,10 @@ def call(){
       sh "docker run -td centos 2> /dev/null"
       }
   }
+  stage "check Docker Image", {
+    node{
+      println "docker: execute: login_to_registry()"
+      sh "docker ps -a"
+      }
+  }
 }
