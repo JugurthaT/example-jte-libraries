2f29da9a4157dad77bcf80e7d87cee2c78733e79
JugurthaT <aimen_r@yahoo.fr>
2019-12-05 17:05:04 +0300

    Update static_code_analysis.groovy

--- sonarqube/static_code_analysis.groovy
+++ sonarqube/static_code_analysis.groovy
@@ -1,14 +1,12 @@
 void call(){
 
-        stage "Example code analysis" {
+        stage "Example code analysis", {
                 node{
       unstash "workspace"
 
       println "docker: execute: login_to_registry()"
-      sh "docker run -td centos 2> /dev/null"
+      sh "docker ps -a 2> /dev/null"
       }
     }
 }
-
- 
 
